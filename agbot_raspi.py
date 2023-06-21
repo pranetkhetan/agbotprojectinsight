353572a3ec2b06d170bace98c923cf35c368d99a
pranetkhetan <109355228+pranetkhetan@users.noreply.github.com>
2023-06-21 09:41:23 +0530

    initial commit

---
+++ agbot_raspi.py
@@ -0,0 +1,35 @@
+import curses
+import serial
+import time
+
+arduino = serial.Serial('COM9',9600,timeout=1)
+time.sleep(0.02)
+
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
+
+try:
+    while True:
+        time.sleep(0.15)
+        char = screen.getch()
+        if char == ord('q'):
+            break
+        elif char == curses.KEY_UP:
+            arduino.write(b'f')
+        elif char == curses.KEY_DOWN:
+            arduino.write(b'b')
+        elif char == curses.KEY_RIGHT:
+            arduino.write(b'r')
+        elif char == curses.KEY_LEFT:
+            arduino.write(b'l')
+        elif char == ord(' '):
+            arduino.write(b's')
+        else:
+            arduino.write(b's')
+finally:
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
+
+
---
+++ projectinsight.ino
@@ -0,0 +1,100 @@
+#define lft_R_IS 4
+#define lft_R_EN 2
+#define lft_R_PWM 5
+#define lft_L_IS 7
+#define lft_L_EN 1
+#define lft_L_PWM 3
+
+#define rgt_R_IS 13
+#define rgt_R_EN 12
+#define rgt_R_PWM 6
+#define rgt_L_IS 8
+#define rgt_L_EN 11
+#define rgt_L_PWM 10
+
+int pwmspeed;
+char command;
+
+void setup(){
+//LEFTMOTORDRIVER
+pinMode(lft_R_IS,OUTPUT);
+pinMode(lft_R_EN,OUTPUT);
+pinMode(lft_R_PWM,OUTPUT);
+pinMode(lft_L_IS,OUTPUT);
+pinMode(lft_L_EN,OUTPUT);
+pinMode(lft_L_PWM,OUTPUT);
+digitalWrite(lft_R_IS,LOW);
+digitalWrite(lft_L_IS,LOW);
+digitalWrite(lft_R_EN,HIGH);
+digitalWrite(lft_L_EN,HIGH);
+
+//RIGHTMOTORDRIVER
+pinMode(rgt_R_IS,OUTPUT);
+pinMode(rgt_R_EN,OUTPUT);
+pinMode(rgt_R_PWM,OUTPUT);
+pinMode(rgt_L_IS,OUTPUT);
+pinMode(rgt_L_EN,OUTPUT);
+pinMode(rgt_L_PWM,OUTPUT);
+digitalWrite(rgt_R_IS,LOW);
+digitalWrite(rgt_L_IS,LOW);
+digitalWrite(rgt_R_EN,HIGH);
+digitalWrite(rgt_L_EN,HIGH);
+
+//SERIALCOMMS
+Serial.begin(9600);
+delay(2000);
+}
+
+void forward(int pwmspeed){
+analogWrite(rgt_R_PWM,pwmspeed);
+analogWrite(rgt_L_PWM,0);
+analogWrite(lft_R_PWM,pwmspeed);
+analogWrite(lft_L_PWM,0);
+}
+void right(int pwmspeed){
+analogWrite(rgt_R_PWM,pwmspeed);
+analogWrite(rgt_L_PWM,0);
+analogWrite(lft_R_PWM,0);
+analogWrite(lft_L_PWM,pwmspeed);
+}
+void left(int pwmspeed){
+analogWrite(rgt_R_PWM,0);
+analogWrite(rgt_L_PWM,pwmspeed);
+analogWrite(lft_R_PWM,pwmspeed);
+analogWrite(lft_L_PWM,0);
+}
+void backward(int pwmspeed){
+analogWrite(rgt_R_PWM,0);
+analogWrite(rgt_L_PWM,pwmspeed);
+analogWrite(lft_R_PWM,0);
+analogWrite(lft_L_PWM,pwmspeed);
+}
+
+void stopmotors(){
+analogWrite(rgt_R_PWM,0);
+analogWrite(rgt_L_PWM,0);
+analogWrite(lft_R_PWM,0);   
+analogWrite(lft_L_PWM,0);              
+}
+
+void loop(){
+  delay(50);
+  if(Serial.available() > 0) {
+    command = Serial.read();
+    if(command == 'f'){
+      forward(200);
+    } else if(command == 'b') {
+      backward(200);
+    } else if(command == 'r') {
+      right(200);
+    } else if(command == 'l') {
+      left(200);
+    } else if(command == 's') {
+      stopmotors();
+    } else {
+      stopmotors();
+    }
+  } else {
+    stopmotors();
+  }
+}
